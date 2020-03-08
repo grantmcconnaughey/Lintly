c4715ff6ada4786723196da701b851bd1ed6e69b
Grant McConnaughey <grantmcconnaughey@gmail.com>
2020-03-07 23:01:41 -0600

    Fix spacing

--- lintly/violations.py
+++ lintly/violations.py
@@ -4,8 +4,10 @@ class Violation(object):
         self.column = column
         self.code = code
         self.message = message
+
     def __str__(self):
         return self.message
+
     def __repr__(self):
         return 'Violation(line={}, column={}, code="{}", message="{}")'.format(
                         self.line, self.column, self.code, self.message)
